b36ad0363a4931d6ac81bc8c7316139cdd861030
Daniel Deng <haochendeng2000@berkeley.edu>
2024-01-18 12:32:18 -0800

    a

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email="hdeng3@caltech.edu",
     description="Collection of neural analysis tools",
     url="https://github.com/ddeng00/neural_analysis.git",
-    packages=find_packages(),
+    packages=["neural_analysis", "neural_analysis.new"],
     python_requires=">=3.10",
     install_requires=[
         "numpy",
